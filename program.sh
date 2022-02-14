4c167e6d87bad73cae449444cc5e9ffed83f0e6a
Safi-Bouziane <78704181+Safi-Bouziane@users.noreply.github.com>
2022-02-14 17:06:25 +0100

    Update program.sh

--- program.sh
+++ program.sh
@@ -4,7 +4,6 @@ echo "Hello World!";
 echo "Feature 2";
 read -p "Give your name: " name;
 echo $name;
-echo "5bd20d8"
 echo "Hello $name. Greetings to you my friend";
 echo "hallo mandarijnen zijn lekker"
 echo "Hello $name. Greetings to you my friend.";
